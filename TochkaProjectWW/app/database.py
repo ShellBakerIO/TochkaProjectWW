9037c19ae55106135ff6932a880521fb56e333a7
Fariz Nasibov <shellbaker123@gmail.com>
2025-05-31 16:32:51 +0500

    change db url

--- TochkaProjectWW/app/database.py
+++ TochkaProjectWW/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://nasibov:123@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres1:postgres123@localhost:5432/WWExchange"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
